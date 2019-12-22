60aeaf54c15b27215d200f16b774fc32f0f50593
momokellyz <59138446+momokellyz@users.noreply.github.com>
2019-12-22 20:45:12 +0700

    Update esphop.js

--- esphop.js
+++ esphop.js
@@ -201,62 +201,18 @@ function cripple_window(_window) {
             let calcDistanceTo = function(player) {return getD3D(player.x3, player.y3, player.z3, me.x, me.y, me.z)};
             let isCloseEnough = function(player) {let distance = calcDistanceTo(player); return me.weapon.range >= distance && ("Shotgun" != me.weapon.name || distance < 70) && ("Akimbo Uzi" != me.weapon.name || distance < 100);};
             let haveAmmo = function() {return !(me[ammos][me[weaponIndex]] !== undefined && me[ammos][me[weaponIndex]] == 0);};
-
-            // target selector - based on closest to aim
-            let closest = null, closestAngle = Infinity;
-            let players = world.players.list;
-            for (let i = 0; me.active && i < players.length; i++) {
-                let e = players[i];
-                if (e[isYou] || !e.active || !e[objInstances] || !isEnemy(e)) {
-                    continue;
-                }
-
-                // experimental prediction removed
-                e.x3 = e.x;
-                e.y3 = e.y;
-                e.z3 = e.z;
-
-                if (!isCloseEnough(e) || !canHit(e)) {
-                    continue;
-                }
-
-                let angle = calcAngleTo(e);
-                if (angle < closestAngle) {
-                    closestAngle = angle;
-                    closest = e;
-                }
-            }
-            // aimbot
-            let ty = controls.object.rotation.y, tx = controls[pchObjc].rotation.x;
-            if (closest) {
-                let target = closest;
-                let y = target.y3 + playerHeight - (headScale/* + hitBoxPad*/) / 2 - target.crouchVal * crouchDst;
-                if (me.weapon[nAuto] && me[didShoot]) {
-                    inputs[SHOOT] = 0;
-                } else if (!me.aimVal) {
-                    inputs[SHOOT] = 1;
-                    inputs[SCOPE] = 1;
-                } else {
-                    inputs[SCOPE] = 1;
-                }
-
-                ty = getDir(controls.object.position.z, controls.object.position.x, target.z3, target.x3);
-                tx = getXDire(controls.object.position.x, controls.object.position.y, controls.object.position.z, target.x3, y, target.z3);
-
-                // perfect recoil control
-                tx -= .3 * me[recoilAnimY];
-            } else {
-                inputs[SHOOT] = controls[mouseDownL];
-                inputs[SCOPE] = controls[mouseDownR];
-            }
+		
+            // silent aim
+            inputs[xDr] = +(tx % PI2).toFixed(3);
+            inputs[yDr] = +(ty % PI2).toFixed(3);
 
 
-				
             // auto reload
-		controls.keys[controls.reloadKey] = !haveAmmo() * 1;
-	
-	    // bhop
-		inputs[JUMP] = (controls.keys[controls.jumpKey] && !me[didJump]) * 1;
+            controls.keys[controls.reloadKey] = !haveAmmo() * 1;
+
+            // bhop
+            inputs[JUMP] = (controls.keys[controls.jumpKey] && !me[didJump]) * 1;
+
 			
             // runs once
             if (!shared_state.get('init')) {
